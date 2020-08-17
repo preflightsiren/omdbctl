c37708fac4a8ac502c8e1be7fa34c72de689aaf2
Sebastian Cole <sebastian.cole@10xbanking.com>
2020-08-17 22:36:46 +1000

    Adding checks around running

--- cmd/omdbctl/main.go
+++ cmd/omdbctl/main.go
@@ -10,6 +10,18 @@ import (
 func main() {
 	fmt.Printf("omdbctl - the Open Movie DB search tool.")
 	// Validate the input
+	if os.Getenv("APIKEY") == "" {
+		fmt.Println("Environmant variable APIKEY not set. Please visit http://www.omdbapi.com/apikey.aspx to obtain new API keys.")
+		return
+	}
+	if len(os.Args) != 2 {
+		printHelp()
+		return
+	}
 	omdb := omdbctl.NewOMDB(os.Getenv("APIKEY"))
-	omdb.Search("tt0113243")
+	omdb.Search(os.Args[1])
+}
+
+func printHelp() {
+	fmt.Printf(`Usage: %s [ID|title|search term]`, os.Args[0])
 }
