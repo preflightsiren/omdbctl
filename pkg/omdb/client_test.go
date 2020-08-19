b3e393009189bee50fbc8c20e93f62f0f17024bf
Sebastian Cole <sebastian.cole@10xbanking.com>
2020-08-19 22:26:31 +1000

    Updating unit tests

--- pkg/omdb/client_test.go
+++ pkg/omdb/client_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,23 +36,24 @@ func TestGetMovieByID(t *testing.T) {
 	}
 
 	actualMovieResponse, err := testClient.GetMovieByID("12345")
+	posterURL, _ := url.Parse("https://m.media-amazon.com/images/M/MV5BNmExMTkyYjItZTg0YS00NWYzLTkwMjItZWJiOWQ2M2ZkYjE4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg")
 
-	expectedMovie := &OMDBResponse{
+	expectedMovie := &Movie{
 		ID:       "tt0113243",
 		Title:    "Hackers",
-		Year:     "1995",
+		Year:     1995,
 		Rated:    "PG-13",
 		Released: "15 Sep 1995",
 		Runtime:  "105 min",
-		Genre:    "Comedy, Crime, Drama, Thriller", //[]string{"Comedy", "Crime", "Drama", "Thriller"},
+		Genre:    []string{"Comedy", "Crime", "Drama", "Thriller"},
 		Director: "Iain Softley",
 		Writer:   "Rafael Moreu",
-		Actors:   "Jonny Lee Miller, Angelina Jolie, Jesse Bradford, Matthew Lillard", //[]string{"Jonny Lee Miller", "Angelina Jolie", "Jesse Bradford", "Matthew Lillard"},
+		Actors:   []string{"Jonny Lee Miller", "Angelina Jolie", "Jesse Bradford", "Matthew Lillard"},
 		Plot:     "Hackers are blamed for making a virus that will capsize five oil tankers.",
-		Language: "English, Italian, Japanese, Russian", //[]string{"English", "Italian", "Japanese", "Russian"},
+		Language: []string{"English", "Italian", "Japanese", "Russian"},
 		Country:  "USA",
 		Awards:   "N/A",
-		Poster:   "https://m.media-amazon.com/images/M/MV5BNmExMTkyYjItZTg0YS00NWYzLTkwMjItZWJiOWQ2M2ZkYjE4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+		Poster:   posterURL,
 		Ratings: []*Rating{
 			&Rating{
 				Source: "Internet Movie Database",
@@ -66,16 +68,6 @@ func TestGetMovieByID(t *testing.T) {
 				Value:  "46/100",
 			},
 		},
-		// "Metascore":"46",
-		// "imdbRating":"6.3",
-		// "imdbVotes":"64,262",
-		// "imdbID":"tt0113243",
-		// "Type":"movie",
-		// "DVD":"24 Apr 2001",
-		// "BoxOffice":"N/A",
-		// "Production":"MGM",
-		// "Website":"N/A",
-		Response: "True",
 	}
 
 	assert.Nil(t, err)
